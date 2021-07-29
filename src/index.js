2d5f48b66a8105c8aa61a0bde966d49a953f95bd
Louis Lassegue <louislassegue@gmail.com>
2021-07-28 19:00:16 -0700

    added auth and login

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 import App from './app.js';
 import ThemeProvider from './components/context/Theme.js'
+import AuthProvider from '../src/components/context/Auth.js';
 import "normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
@@ -11,9 +12,11 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 class Main extends React.Component {
   render() {
     return (
+      <AuthProvider>
       <ThemeProvider>
         <App />
       </ThemeProvider>
+      </AuthProvider>
     )
   }
 }
